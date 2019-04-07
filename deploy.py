2f1d17a50df281aac5a9f809ff2f48400f137e68
Puneet Kumar <ebuddiess@gmail.com>
2019-04-07 19:52:47 +0530

    S

--- deploy.py
+++ deploy.py
@@ -32,18 +32,17 @@ def predict():
 
 if __name__ == '__main__':
     def processing(url):
-    
-    tokens_slash = str(url.encode('utf-8')).split('/')# make tokens after splitting by slash
-    total_Tokens = []
-    for i in tokens_slash:
-        tokens = str(i).split('-')# make tokens after splitting by dash
-        tokens_dot = []
-        for j in range(0,len(tokens)):
-            temp_Tokens = str(tokens[j]).split('.')# make tokens after splitting by dot
-            tokens_dot = tokens_dot + temp_Tokens
-        total_Tokens = total_Tokens + tokens + tokens_dot
-    finaltest = list(set(total_Tokens))#remove redundant tokens
-    return finaltest 
+        tokens_slash = str(url.encode('utf-8')).split('/')# make tokens after splitting by slash
+        total_Tokens = []
+        for i in tokens_slash:
+            tokens = str(i).split('-')# make tokens after splitting by dash
+            tokens_dot = []
+            for j in range(0,len(tokens)):
+                temp_Tokens = str(tokens[j]).split('.')# make tokens after splitting by dot
+                tokens_dot = tokens_dot + temp_Tokens
+            total_Tokens = total_Tokens + tokens + tokens_dot
+        finaltest = list(set(total_Tokens))#remove redundant tokens
+        return finaltest 
     vectorizer = joblib.load("vectorizer.pkl")
     
     app.run()
