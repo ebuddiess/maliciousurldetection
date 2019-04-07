1f9a42d6cd331293e83553aa3ac8b7fa66c965e6
Puneet Kumar <ebuddiess@gmail.com>
2019-04-07 20:50:57 +0530

    S

--- deploy.py
+++ deploy.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify , render_template ,Response ,send_file 
-from flask import current_app as capp
+from flask import Flask, request, jsonify , render_template ,Response ,send_file
 import pickle
 import  process as p
 from sklearn.externals import joblib
@@ -9,7 +8,22 @@ from scipy.sparse import hstack
 
 app = Flask(__name__)
 
+def processing(url):
+    
+    tokens_slash = str(url.encode('utf-8')).split('/')# make tokens after splitting by slash
+    total_Tokens = []
+    for i in tokens_slash:
+        tokens = str(i).split('-')# make tokens after splitting by dash
+        tokens_dot = []
+        for j in range(0,len(tokens)):
+            temp_Tokens = str(tokens[j]).split('.')# make tokens after splitting by dot
+            tokens_dot = tokens_dot + temp_Tokens
+        total_Tokens = total_Tokens + tokens + tokens_dot
+    finaltest = list(set(total_Tokens))#remove redundant tokens
+    return finaltest 
+
 rfc = joblib.load("randomforestfinal.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
 
 @app.route('/download',methods=['GET'])
 def download():
@@ -19,31 +33,15 @@ def download():
 def main():
     return render_template('index.html',websitename="Maltracker");
 
-vectorizer = ""
-
-if __name__ == '__main__':
-    def processing(url):
-        tokens_slash = str(url.encode('utf-8')).split('/')# make tokens after splitting by slash
-        total_Tokens = []
-        for i in tokens_slash:
-            tokens = str(i).split('-')# make tokens after splitting by dash
-            tokens_dot = []
-            for j in range(0,len(tokens)):
-                temp_Tokens = str(tokens[j]).split('.')# make tokens after splitting by dot
-                tokens_dot = tokens_dot + temp_Tokens
-            total_Tokens = total_Tokens + tokens + tokens_dot
-        finaltest = list(set(total_Tokens))#remove redundant tokens
-        return finaltest 
-    global vectorizer
-    vectorizer = joblib.load("vectorizer.pkl")
-    app.run()
-
 @app.route('/api',methods=['GET'])
 def predict():
-         params = request.args.get('url')
-         testapi = vectorizer.transform([params])
-         n = p.feature_processing(params)
-         n = sp.sparse.csr_matrix(n)
-         t = hstack([testapi,n])
-         data = rfc.predict(t);
-         return  jsonify(status=(data[0]))
+    params = request.args.get('url')
+    testapi = vectorizer.transform([params])
+    n = p.feature_processing(params)
+    n = sp.sparse.csr_matrix(n)
+    t = hstack([testapi,n])
+    data = rfc.predict(t);
+    return  jsonify(status=(data[0]))
+
+if __name__ == '__main__':
+   app.run()
